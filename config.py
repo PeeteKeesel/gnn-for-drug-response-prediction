bf05a0d3c9289b54500f012861fae0959050d3b7
PeeteKeesel <68206125+PeeteKeesel@users.noreply.github.com>
2022-08-03 12:53:13 +0200

    started to build baseline DNN including gene expr, cnv and drug information

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ PATH_TO_SAVE_STRING_DATA_TO = '../../datasets/string/my_datasets/'
 GENE_EXPR_FINAL_FILE_NAME = 'joined_gdsc_geneexpr.pkl'
 CNV_GISTIC_FINAL_FILE_NAME = 'joined_gdsc_cnv_gistic.pkl'
 CNV_PICNIC_FINAL_FILE_NAME = 'joined_gdsc_cnv_picnic.pkl'
+DRUG_FPS_FINAL_FILE_NAME = 'drug_name_fingerprints_dataframe.pkl'
 
 # Specific paths.
 PATH_TO_SAVED_DRUG_FEATURES = '../../datasets/gdsc/my_datasets/'
+PATH_TO_SAVED_CL_FEATURES = '../../datasets/gdsc/my_datasets/'
--- docs/what_to_read.md
+++ docs/what_to_read.md
@@ -1,2 +1,6 @@
 
-- 
+
+
+# DL for DRP
+
+- https://bmcbioinformatics.biomedcentral.com/articles/10.1186/s12859-021-04352-9 
